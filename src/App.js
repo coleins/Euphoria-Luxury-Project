30f944bb1aa503bca6862c13c9d3987d2a889537
Collins Ngetich <collinsngetichk@gmail.com>
2024-05-11 21:26:00 +0300

    modifies components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import React from "react";
 import Contact from "./components/Contact";
-import About from "./components/About";
-import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Contact />
-      <About />
-      <Home />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
