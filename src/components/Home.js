82539df2975ffc84b4a841be8820a02b97240cb8
dorcas <dorcasaumaakamu@gmail.com>
2024-05-11 21:10:46 +0300

    modifies components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div style={{ margin: "200px", padding: "20px" }}>
+        <h1
+          style={{
+            color: "white",
+            backgroundColor: "#232323",
+            paddingBottom: "30px",
+            paddingTop: "30px",
+            textAlign: "center",
+            padding: "20px",
+          }}
+        >
+          HOME
+        </h1>
+
+        <p>
+          Established in 2010 by Dorcas Akamuran, David Mugambi, Catherine
+          Wanjiku, and Alex Karanja, Euphoria Luxury epitomizes a harmonious
+          fusion of opulence and hospitality. Nestled at the pinnacle of the
+          hospitality industry, our mission is to redefine luxury accommodation,
+          offering guests an unparalleled experience in a realm of refined
+          elegance and unparalleled comfort.
+          <br /> From the moment guests step through our doors, they are
+          enveloped in an atmosphere of sophistication and tranquility,
+          meticulously curated to surpass expectations. Our commitment to
+          excellence permeates every aspect of the Euphoria experience, from our
+          sumptuously appointed rooms and world-class amenities to our
+          impeccable service delivered by a dedicated team of professionals.
+          <br /> Driven by innovation and a passion for hospitality, we
+          continually strive to set new standards in the industry, ensuring that
+          each guest leaves with cherished memories and a longing to return to
+          the unparalleled oasis of luxury that is Euphoria Luxury Hotel..
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
